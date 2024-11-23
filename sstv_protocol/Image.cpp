4ce26ec4cde8963ae370b08dad68ee8b18e3402c
Lior G <liorgleizer1@gmail.com>
2024-11-23 14:17:03 +0200

    code: Image class

---
+++ sstv_protocol/Image.cpp
@@ -0,0 +1,82 @@
+#include "Image.hpp"
+
+
+Image::Image(const std::string& path)
+	:_image(cv::imread(path))
+{
+  _width = _image.cols;
+  _height = _image.rows;
+  _channels = _image.channels();
+}
+
+void Image::resize(const cv::Mat& inImage, cv::Mat& outImage, int width, int height)
+{
+  cv::resize(inImage, outImage, cv::Size(width, height));
+}
+
+std::vector<uchar> Image::getPixelsGray() const
+{
+  cv::Mat grayImage;
+  cv::cvtColor(_image, grayImage, cv::COLOR_BGR2GRAY);
+
+  std::vector<uchar> pixels(_width * _height);
+  for (int i = 0; i < _height; i++) {
+    for (int j = 0; j < _width; j++) {
+      pixels[i * _width + j] = grayImage.at<uchar>(i, j);
+    }
+  }
+  return pixels;
+}
+
+std::vector<uchar> Image::getPixelsGray(int width, int height) const
+{
+  cv::Mat grayImage;
+  cv::Mat resized;
+  cv::cvtColor(_image, grayImage, cv::COLOR_BGR2GRAY);
+
+  resize(grayImage, resized, width, height);
+
+  std::vector<uchar> pixels(width * height);
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      pixels[i * width + j] = resized.at<uchar>(i, j);
+    }
+  }
+  return pixels;
+}
+
+std::vector<cv::Vec3b> Image::getPixelsColor() const
+{
+  if (_channels != 3) {
+    throw std::runtime_error("Image is not a 3-channel color image");
+  }
+
+  std::vector<cv::Vec3b> pixels(_width * _height);
+  for (int i = 0; i < _height; i++) {
+    for (int j = 0; j < _width; j++) {
+      pixels[i * _width + j] = _image.at<cv::Vec3b>(i, j);
+    }
+  }
+  return pixels;
+}
+
+std::vector<cv::Vec3b> Image::getPixelsColor(int width, int height) const
+{
+  cv::Mat resized;
+  if (_channels != 3) {
+    throw std::runtime_error("Image is not a 3-channel color image");
+  }
+
+  resize(_image, resized, width, height);
+
+  std::vector<cv::Vec3b> pixels(width * height);
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      pixels[i * width + j] = resized.at<cv::Vec3b>(i, j);
+    }
+  }
+  return pixels;
+}
+
+
+
---
+++ sstv_protocol/Image.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <opencv2/opencv.hpp>
+
+class Image {
+private:
+	cv::Mat _image;
+	int _width;
+	int _height;
+	int _channels;
+
+public:
+	Image(const std::string& path);
+
+	static void resize(const cv::Mat& inImage, cv::Mat& outImage, int width, int height);
+
+	std::vector<uchar> getPixelsGray() const;
+	std::vector<uchar> getPixelsGray(int width, int height) const;
+
+	std::vector<cv::Vec3b> getPixelsColor() const;
+	std::vector<cv::Vec3b> getPixelsColor(int width, int height) const;
+};
